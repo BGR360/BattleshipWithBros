7caa2014c36c00da98660f3af3f121e39a79a8dd
Ben Reeves <benwolverine2019@gmail.com>
2016-01-07 14:14:31 -0500

    Added Coordinate class

---
+++ BattleshipTest/src/battleship/Coordinate.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Ben on 1/7/16.
+ *
+ * Represents a location on a Battleship board, such as "B4" or "I8"
+ * Internally stores the row as a number but can use letters and numbers interchangeably.
+ */
+
+package battleship;
+
+public class Coordinate
+{
+}

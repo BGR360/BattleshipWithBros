ef8c4d0d41cdeb44cf8670ae16b0adb03ef9dc5e
Ben Reeves <benwolverine2019@gmail.com>
2016-01-07 14:58:19 -0500

    Added CoordinateTest

---
+++ BattleshipTest/src/test/battleship/CoordinateTest.java
@@ -0,0 +1,52 @@
+package battleship;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Ben on 1/7/16.
+ *
+ */
+public class CoordinateTest {
+
+    @Test
+    public void testDefaultConstruct() throws Exception {
+
+    }
+
+    @Test
+    public void testStringConstruct() throws Exception {
+
+    }
+
+    @Test
+    public void testLetterNumConstruct() throws Exception {
+
+    }
+
+    @Test
+    public void testNumNumConstruct() throws Exception {
+
+    }
+
+    @Test
+    public void testNumToLetter() throws Exception {
+
+    }
+
+    @Test
+    public void testLetterToNum() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+}
